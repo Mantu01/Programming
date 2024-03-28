be533c6e05563f08b768562fcccf92e66c35fcad
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-03-28 23:30:23 +0530

    create C++ Code/Basics/Array/1 D/Pair of sum K.cpp

---
+++ C++ Code/Basics/Array/1 D/Pair of sum K.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,k;
+    cout<<"K = ";
+    cin>>k;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the value of elements : "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    cout<<"Pair of indices  ";    
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i]+arr[j]==k)
+                cout<<"("<<i<<","<<j<<") ,  ";
+        }
+    }
+    cout<<"whose element's sum is equal to K = "<<k;
+    return 0;
+}

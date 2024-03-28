e7c9c90773d7df042bfa10e05505152fec9d59d6
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-03-28 23:34:46 +0530

    create C++ Code/Basics/Array/1 D/Triplet of sum K.cpp

---
+++ C++ Code/Basics/Array/1 D/Triplet of sum K.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,K;
+    cout<<"K = ";
+    cin>>K;
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the value of elements : "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    cout<<"Triplet of indices  ";    
+    for(int i=0;i<n-1;i++){
+        for(int j=i+1;j<n;j++){
+            for(int k=j+1;k<n;k++){
+                if(arr[i]+arr[j]+arr[k]==K)
+                    cout<<"("<<i<<","<<j<<","<<k<<") ,  ";
+            }
+        }
+    }
+    cout<<"whose element's sum is equal to K = "<<K;
+    return 0;
+}

d5eb88485caa414c78a3a735a1c8e0b8ffaeba8a
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-03-28 23:37:58 +0530

    create C++ Code/Basics/Array/2 D/Element's Product.cpp

---
+++ C++ Code/Basics/Array/2 D/Element's Product.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int r,c;
+    cout<<"Enter the number of rows : ";
+    cin>>r;
+    cout<<"Enter the number of columns : ";
+    cin>>c;
+    int arr[r][c];
+    cout<<"Enter the elements "<<endl;
+    for(int i=0;i<r;i++){
+        cout<<endl<<"for row "<<i+1<<endl;
+        for(int j=0;j<c;j++){
+            cout<<"in column "<<j+1<<" : ";
+            cin>>arr[i][j];
+        }
+    }
+    int product=1;
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++)
+            product*=arr[i][j];
+    }
+    cout<<endl<<"Sum of the elements : "<<product;
+    return 0;
+}

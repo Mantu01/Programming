f1de4b79573fd40defcd3da9df00622b78b8d4ab
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-03-28 23:38:31 +0530

    create C++ Code/Basics/Array/2 D/Elements greater than 'K'.cpp

---
+++ C++ Code/Basics/Array/2 D/Elements greater than 'K'.cpp
@@ -0,0 +1,25 @@
+// Q. Find all the elements in a 2D array which is greater the 'K'.
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,K;
+    cout<<"Enter K = ";
+    cin>>K;
+    cout << "Enter the number of rows and columns :" << endl;
+    cin>>a>>b;
+    int arr[a][b];
+    cout << "Enter the elements :" << endl;
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++)
+            cin>>arr[i][j];
+    }
+    cout<<"Elements of ";
+    for(int i=0;i<a;i++){
+        for(int j=0;j<b;j++){
+            if(arr[i][j]>K)
+                cout<<"( row "<<i<<", column "<<j<<") , ";
+        }
+    }
+    cout<<"of given 2D array are greater than K = "<<K;
+    return 0;
+}

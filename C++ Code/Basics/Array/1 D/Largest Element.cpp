400b7bb467ae2a6ef4884a7218013108c37af987
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-03-28 23:29:39 +0530

    create C++ Code/Basics/Array/1 D/Largest Element.cpp

---
+++ C++ Code/Basics/Array/1 D/Largest Element.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,ans[2]={INT_MIN,0};
+    cout<<"Enter the size of array : ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the value of elements : "<<endl;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    for(int i=0;i<n;i++){
+        if(arr[i]>ans[0]){
+            ans[0]=arr[i];
+            ans[1]=i;
+        }
+    }
+    cout<<"Element of the value "<<ans[0]<<" in index "<<ans[1]<<" is the largest element.";
+    return 0;
+}

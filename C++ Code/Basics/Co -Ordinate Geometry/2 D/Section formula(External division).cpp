1d065c30be594d3b84fd8aed31c8c2631cd5543f
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-03-29 09:29:41 +0530

    create C++ Code/Basics/Co -Ordinate Geometry/2 D/Section formula(External division).cpp

---
+++ C++ Code/Basics/Co -Ordinate Geometry/2 D/Section formula(External division).cpp
@@ -0,0 +1,26 @@
+/* Find the the coordinates of the point which divides the the line joining two points 
+    in ratio m:n externally.Coordinates of the points value of m and n are given by the user.      */
+#include<iostream>
+using namespace std;
+int main(){
+    float x1,y1,x2,y2,m,n;
+    cout<<"For the first point."<<endl<<"Enter the X-coordinate : ";
+    cin>>x1;
+    cout<<"Enter the Y-coordinate : ";
+    cin>>y1;
+    cout<<endl<<"For the second point."<<endl<<"Enter the X-coordinate : ";
+    cin>>x2;
+    cout<<"Enter the Y-coordinate : ";
+    cin>>y2;
+    cout<<"Let the required points devid";
+    cout<<"Coordinates of the point which divides the line in the ratio m:n."<<endl<<"Enter m : ";
+    cin>>m;
+    cout<<"Enter n : ";
+    cin>>n;
+    int x3,y3;        // coordinates of required point.
+    x3=(x2*m-x1*n)/(m-n);
+    y3=(y2*m-y1*n)/(m-n);
+    cout<<endl<<"("<<x3<<","<<y3<<") divides the line joining ("<<x1<<","<<y1<<") and ("<<x2
+    <<","<<y2<<") in ratio "<<m<<":"<<n<<" externally.";
+    return 0;
+}

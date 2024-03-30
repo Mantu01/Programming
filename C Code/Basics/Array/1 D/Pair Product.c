a5d1bbcd12e1636cb1b539e6956611d0a70aae34
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-03-30 11:22:36 +0530

    update C Code/Basics/Array/1 D/Pair Product.c

---
+++ C Code/Basics/Array/1 D/Pair Product.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+  int n;
+  printf("Enter the size of array : ");
+  scanf("%d",&n);
+  int arr[n];
+  printf("Enter all the elements :\n");
+  for(int i=0;i<n;i++)
+    scanf("%d",&arr[i]);
+  int k;
+  printf("Enter Key : ");
+  scanf("%d",&k);
+  printf("\nPair elements whose product is equal to %d :\n",k);
+  for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+      if(arr[i]*arr[j]==k)
+        printf("(%d,%d) ",arr[i],arr[j]);
+    }
+  }
+  return 0;
+}

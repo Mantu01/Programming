ada34f084da96f46cab7e2d8c121ece05ff4841c
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-04-02 16:13:51 +0530

    update C Code/Basics/Array/1 D/Merge two sorted array.c

--- C Code/Basics/Array/1 D/Merge two sorted array.c
+++ C Code/Basics/Array/1 D/Merge two sorted array.c
@@ -1,10 +1,4 @@
 #include<stdio.h>
-int sum(int* arr, int k,int n){
-  int sum=0;
-  for(int i=k;i<n;i++)
-    sum+=arr[i];
-  return sum;
-}
 int main(){
   int n1,n2,n3;
   printf("Enter the size of first array : ");

826e37a1a58ca673ba38526ce6d3d4f25800bb46
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-04-02 17:06:23 +0530

    update C Code/Basics/Array/2 D/Element's Sum.c

---
+++ C Code/Basics/Array/2 D/Element's Sum.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+  int r,c;
+  printf("Enter the number of rows : ");
+  scanf("%d",&r);
+  printf("Enter the number of columns : ");
+  scanf("%d",&c);
+  return 0;
+}

9763c462f3a4e84cd452feab3ff6e8eb97b657c3
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-04-02 17:11:48 +0530

    update C Code/Basics/Array/2 D/Element's Sum.c

--- C Code/Basics/Array/2 D/Element's Sum.c
+++ C Code/Basics/Array/2 D/Element's Sum.c
@@ -5,5 +5,17 @@ int main(){
   scanf("%d",&r);
   printf("Enter the number of columns : ");
   scanf("%d",&c);
+  int arr[r][c];
+  printf("Enter all elements :\n");
+  for(int i=0;i<r;i++){
+    for(int j=0;j<c;j++)
+      scanf("%d",&arr[i][j]);
+  }
+  int sum=0;
+  for(int i=0;i<r;i++){
+    for(int j=0;j<c;j++)
+      sum+=arr[i][j];
+  }
+  printf("Sum of the elements : %d",sum);
   return 0;
 }

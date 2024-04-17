ef379305ff8858176b378030bf4ee3ff0451e760
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-04-17 12:08:00 +0530

    update C++ Code/Data Structure/Linked List/Singly Linked List/Insertion.cpp

--- C++ Code/Data Structure/Linked List/Singly Linked List/Insertion.cpp
+++ C++ Code/Data Structure/Linked List/Singly Linked List/Insertion.cpp
@@ -1,4 +1,4 @@
-// WAP to insert node at Kth position in singly linked list.
+// WAP to insert and delete node at Kth position in singly linked list.
 #include<iostream>
 using namespace std;
 class node{
@@ -45,24 +45,57 @@ void insert(int data, int p, node* &head){
   temp->next=curr->next;
   curr->next=temp;
 }
-int main(){
-  char x;
-  cout<<"Press Y/y for insertion operation else to terminate execution : ";
-  cin>>x;
-  node* head=NULL;
-  cout<<endl<<"Size of current linked list : "<<getLength(head)<<endl<<endl;
-  while(x=='y' || x=='Y'){
-    int data,p;
+void insertion(node* &head){
+  int data,p;
     cout<<"Enter element's data to insert : ";
     cin>>data;
     cout<<"Enter the position to insett the element : ";
     cin>>p;
     insert(data,p,head);
+}
+void deletion(node* &head){
+  int p;
+  cout<<"Enter the Position to delete : ";
+  cin>>p;
+  if(p>getLength(head) || p<1){
+    cout<<p<<" position doesn't exist in the LL to delete."<<endl;
+    return ;
+  }
+  node* temp=NULL;
+  if(p==1){
+    temp=head;
+    head=head->next;
+    temp->next=NULL;
+    delete temp;
+    return ;
+  }
+  node* curr=head;
+  while((p--)>2)
+    head=head->next;
+  temp=curr->next;
+  curr->next=temp->next;
+  temp->next=NULL;
+  delete temp;
+}
+int main(){
+  char a='y';
+  node* head=NULL;
+  cout<<endl<<"Size of current linked list : "<<getLength(head)<<endl<<endl;
+  while(a=='y' || a=='Y'){
+    int x;
+    cout<<"Enter 1 for insertion and 2 for deletion : ";
+    cin>>x;
+    if(x==1)
+      insertion(head);
+    else if(x==2)
+      deletion(head);
+    else
+      cout<<endl<<"Invalid Option."<<endl;
     cout<<"Current Linked List :";
     print(head);
     cout<<endl<<"Size of current linked list : "<<getLength(head)<<endl<<endl;
     cout<<"Press Y/y for insertion operation else to terminate execution : ";
-    cin>>x;
+    cin>>a;
   }
   return 0;
 }

ccb640dec664842262b9ac049a41266375e90c46
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-04-17 12:49:13 +0530

    update C++ Code/Data Structure/Linked List/Singly Linked List/Reverse LL    M ( 1 ).cpp

---
+++ C++ Code/Data Structure/Linked List/Singly Linked List/Reverse LL    M ( 1 ).cpp
@@ -0,0 +1,63 @@
+// WAP to reverse elements of a singly linked list.
+#include<iostream>
+using namespace std;
+class node{
+  public:
+    int data;
+    node* next;
+  node(int d){
+    this->data=d;
+    this->next=NULL;
+  }
+};
+void print(node* head){
+  if(!head)
+    return ;
+  cout<<endl;
+  while(head){
+    cout<<head->data<<" ";
+    head=head->next;
+  }
+  cout<<endl;
+}
+void insert(int data, node* &head, node* &tail){
+  node* temp=new node(data);
+  if(head==NULL){
+    head=temp;
+    tail =temp;
+    return ;
+  }
+  tail->next=temp;
+  tail=temp;
+}
+void reverse(node* &head){
+  if(head==NULL || head->next==NULL)
+    return ;
+  node* curr=head;
+  node* prev=NULL;
+  node* next=NULL;
+  while(curr){
+    next=curr->next;
+    curr->next=prev;
+    prev=curr;
+    curr=next;
+  }
+  head=prev;
+}
+int main(){
+  int data;
+  cout<<"Enter the data (Press -1 to stop insertion) :"<<endl;
+  cin>>data;
+  node* head=NULL;
+  node* tail=NULL;
+  while(data!=-1){
+    insert(data,head,tail);
+    cin>>data;
+  }
+  cout<<endl<<"Given Linked List :";
+  print(head);
+  cout<<endl<<"Reversed Linked Listt :";
+  reverse(head);
+  print(head);
+  return 0;
+}

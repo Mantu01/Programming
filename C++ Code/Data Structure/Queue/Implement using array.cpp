4e87f0f9b36551cf13b4556a93820dfce8a6470d
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-04-20 22:48:10 +0530

    Update Queue

---
+++ C++ Code/Data Structure/Queue/Implement using array.cpp
@@ -0,0 +1,62 @@
+// WAP to implement queue using array.
+#include<iostream>
+using namespace std;
+class queue{
+  public:
+    int start;
+    int end;
+    int* arr;
+    
+  queue(){
+    start=0;
+    end=0;
+    arr=new int[100];
+  }
+  
+  ~queue(){
+    delete []arr;
+  }
+  void push(int data){
+    arr[end]=data;
+    end++;
+  }
+  void pop(){
+    if(start==end)
+      cout<<endl<<"Queue Underflow"<<endl;
+    else
+      start++;
+  }
+  bool isEmpty(){
+    return start==end;
+  }
+  int front(){
+    return arr[start];
+  }
+  int length(){
+    return end-start;
+  }
+};
+int main(){
+  queue q;
+  int x;
+  cout<<"Enter 1 for push or 2 for pop or others for stoping operation : ";
+  cin>>x;
+  while(x==1 || x==2){
+    if(x==1){
+      int data;
+      cout<<"Enter data : ";
+      cin>>data;
+      q.push(data);
+    }
+    else
+      q.pop();
+    cout<<endl<<"Front element : "<<q.front();
+    if(q.isEmpty())
+      cout<<endl<<"Queue is empty.";  
+    else
+      cout<<endl<<"Size of queue : "<<q.length()<<endl;
+    cout<<endl<<"Enter 1 for push or 2 for pop or others for stoping operation : ";
+    cin>>x;
+  }
+  return 0;
+}

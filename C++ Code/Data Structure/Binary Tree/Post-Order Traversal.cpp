25f94fedfc1a156645702e5b0d570ce68c1befc8
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-04-25 16:03:38 +0530

    Update Binary Tree

---
+++ C++ Code/Data Structure/Binary Tree/Post-Order Traversal.cpp
@@ -0,0 +1,47 @@
+// WAP to build a binary tree and print data in Post-Order Traversal using recursion.
+#include<iostream>
+#include<vector>
+using namespace std;
+class node{
+  public:
+    int data;
+    node* left;
+    node* right;
+    
+  node(int d){
+    this->data=d;
+    this->left=NULL;
+    this->right=NULL;
+  }  
+};
+node* build(node* root){
+  int data;
+  cout<<"Enter Data : ";
+  cin>>data;
+  if(data==-1)
+    return root;
+  root=new node(data);
+  cout<<"For left of "<<data<<" , ";
+  root->left=build(root->left);
+  cout<<"For right of "<<data<<" , ";
+  root->right=build(root->right);
+  return root; 
+}
+void POT(vector<int> &arr, node* root){
+  if(!root)
+    return ;
+  POT(arr,root->left);  
+  POT(arr,root->right);
+  arr.push_back(root->data);
+}
+int main(){
+  cout<<"NOTE : Press -1 for leaf node.\n\n";
+  node* root=NULL;
+  root=build(root);
+  vector<int>arr;
+  POT(arr,root);
+  cout<<"\n\nPost-Order Traversal ;\n";
+  for(int i:arr)
+    cout<<i<<" ";
+  return 0;
+}

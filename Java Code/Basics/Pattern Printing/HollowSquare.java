a879ac090c5995e45ac9aaa28e12a2b39c9f113f
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-05-18 21:54:32 +0530

    update Java Code/Basics/Pattern Printing/HollowSquare.java

--- Java Code/Basics/Pattern Printing/HollowSquare.java
+++ Java Code/Basics/Pattern Printing/HollowSquare.java
@@ -1,1 +1,18 @@
- 
+import java.util.*;
+
+public class HollowSquare{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter Size of Side : ");
+    int n=sc.nextInt();
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<=n;j++){
+        if(i==1 || i==n || j==1 || j==n)
+          System.out.print("* ");
+        else
+          System.out.print("  ");
+      }
+      System.out.print("\n");
+    }
+  }
+}

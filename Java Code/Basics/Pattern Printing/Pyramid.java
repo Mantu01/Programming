aff9050f9fd8a78b91d086fe75f52f6d72ef01f3
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-05-19 20:27:10 +0530

    update Java Code/Basics/Pattern Printing/Pyramid.java

---
+++ Java Code/Basics/Pattern Printing/Pyramid.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Pyramid{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter Height : ");
+    int n=sc.nextInt();
+    for(int i=1;i<=n;i++){
+      for(int j=i;j<=n;j++)
+        System.out.print(" ");
+      for(int j=1;j<=i;j++)
+        System.out.print("* ");
+      System.out.print("\n");
+    }
+  }
+}

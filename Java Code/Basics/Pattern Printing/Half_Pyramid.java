7f3e6d1aaad4ed72c227ca38e712eac8940aea77
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-05-19 11:26:01 +0530

    update Java Code/Basics/Pattern Printing/Half_Pyramid.java

--- Java Code/Basics/Pattern Printing/Half_Pyramid.java
+++ Java Code/Basics/Pattern Printing/Half_Pyramid.java
@@ -4,7 +4,16 @@ public class Half_Pyramid{
     Scanner sc=new Scanner(System.in);
     System.out.print("Enter height : ");
     int n=sc.nextInt();
+    System.out.println("Type 1 :");
     for(int i=1;i<=n;i++){
+      for(int j=1;j<=i;j++)
+        System.out.print("* ");
+      System.out.print("\n");
+    }
+    System.out.println("\nType 2 :");
+    for(int i=1;i<=n;i++){
+      for(int j=i;j<n;j++)
+        System.out.print("  ");
       for(int j=1;j<=i;j++)
         System.out.print("* ");
       System.out.print("\n");

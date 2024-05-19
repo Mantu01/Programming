2a215b16de8df9314d0b39355b6170a4a39cfb71
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-05-19 21:32:13 +0530

    update Java Code/Basics/Pattern Printing/SolidRhombus.java

--- Java Code/Basics/Pattern Printing/SolidRhombus.java
+++ Java Code/Basics/Pattern Printing/SolidRhombus.java
@@ -8,12 +8,8 @@ public class SolidRhombus{
     for(int i=1;i<=n;i++){
       for(int j=i;j<n;j++)
         System.out.print("  ");
-      for(int j=1;j<=i;j++)
+      for(int j=1;j<=n;j++)
         System.out.print("*  ");
-      for(int j=i;j<n;j++)
-        System.out.print("*  ");
-      for(int j=1;j<i;j++)
-        System.out.print("  ");
       System.out.print("\n");
     }
   }

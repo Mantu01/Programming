0c30a6f3618487d23e0d05fe4ac5567e13e37ba1
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-05-19 11:31:52 +0530

    update Java Code/Basics/Pattern Printing/Inverted_Half_Pyramid.java

--- Java Code/Basics/Pattern Printing/Inverted_Half_Pyramid.java
+++ Java Code/Basics/Pattern Printing/Inverted_Half_Pyramid.java
@@ -4,7 +4,16 @@ public class Inverted_Half_Pyramid{
     Scanner sc=new Scanner(System.in);
     System.out.print("Enter height : ");
     int n=sc.nextInt();
+    System.out.println("Type 1 :");
     for(int i=1;i<=n;i++){
+      for(int j=i;j<=n;j++)
+        System.out.print("* ");
+      System.out.print("\n");
+    }
+    System.out.println("Type 2 :");
+    for(int i=1;i<=n;i++){
+      for(int j=1;j<i;j++)
+        System.out.print("  ");
       for(int j=i;j<=n;j++)
         System.out.print("* ");
       System.out.print("\n");

e9efb6e19b6f9164e8359cfabfda850d7fb4c144
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-05-20 15:08:54 +0530

    update Java Code/Basics/Other/FibonacciTerm.java

---
+++ Java Code/Basics/Other/FibonacciTerm.java
@@ -0,0 +1,22 @@
+// WAP to check whether a number given by user is pallindrome or not.
+
+import java.util.*;
+
+public class FibonacciTerm{
+  public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the Number : ");
+    int n=sc.nextInt();
+    if(n<1){
+      System.out.println("Number of terms should be greater than 0.");
+      return ;
+    }
+    int ans=1,prev1=1,prev2=0;
+    for(int i=1;i<=n;i++){
+      ans=prev1+prev2;
+      prev2=prev1;
+      prev1=ans;
+    }
+    System.out.println("Term "+n+" of fibonacci series is "+ans);
+  }
+}

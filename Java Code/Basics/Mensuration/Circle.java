2549f3647f7b565871199df9e2bee8ea68bad0c0
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-05-21 00:09:30 +0530

    update Java Code/Basics/Mensuration/Circle.java

--- Java Code/Basics/Mensuration/Circle.java
+++ Java Code/Basics/Mensuration/Circle.java
@@ -1,9 +1,19 @@
 import java.util.*;
 
-public class TemperatureConverter{
+public class Circle{
   public static void main(String[] args){
     Scanner sc=new Scanner(System.in);
-    
-    }
+    System.out.print("Enter the radius : ");
+    float r=sc.nextFloat();
+    System.out.println("For Circle (r = "+r+")");
+    float P=44*r/7;
+    float A=22*r*r/7;
+    System.out.println("Area = "+A);
+    System.out.println("Perimeter = "+P);
+    System.out.println("\nFor Semi-Circle (r = "+r+")");
+    A=A/2;
+    P=P/2+r;
+    System.out.println("Area = "+A);
+    System.out.println("Perimeter = "+P);
   }
 }

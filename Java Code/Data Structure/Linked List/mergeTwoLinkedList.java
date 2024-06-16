035662c756401c121cbe602b52ec1efb90c2ae08
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-06-16 14:10:40 +0530

    update Java Code/Data Structure/Linked List/mergeTwoLinkedList.java

---
+++ Java Code/Data Structure/Linked List/mergeTwoLinkedList.java
@@ -0,0 +1,44 @@
+/*
+  You are given the heads of two sorted linked lists list1 and list2.
+
+  Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+  Return the head of the merged linked list.
+
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+  public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    if(list1==null)
+      return list2;
+    if(list2==null)
+      return list1;
+    ListNode ans=new ListNode(-1);
+    ListNode curr=ans;
+    while(list1!=null && list2!=null){
+      if(list1.val<=list2.val){
+        curr.next=list1;
+        list1=list1.next;
+      }
+      else{
+        curr.next=list2;
+        list2=list2.next;
+      }
+      curr=curr.next;
+    }
+    if(list1!=null)
+      curr.next=list1;
+    if(list2!=null)
+      curr.next=list2;
+    return ans.next;
+  }
+}

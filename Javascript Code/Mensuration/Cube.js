e99fe45cbe79feea612a23c0b97c7335ef6c0d7a
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-07-01 14:50:06 +0530

    update Javascript Code/Mensuration/Cube.js

---
+++ Javascript Code/Mensuration/Cube.js
@@ -0,0 +1,4 @@
+let a=parseInt(prompt("Enter the length of edge of cube : "));
+console.log(`Volume : ${a*a*a} cubic unit.`);
+console.log(`Lateral Surface Area : ${4*a*a} sq unit.`);
+console.log(`Total Surface Area : ${6*a*a} sq unit.`);

5c6073e4d9ba3f50bfee49cfce82f4974559d0d0
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-07-01 12:16:28 +0530

    update Javascript Code/Mensuration/Rectangle.js

---
+++ Javascript Code/Mensuration/Rectangle.js
@@ -0,0 +1,4 @@
+let l=parseInt(prompt("Enter lenght of rectangle : "));
+let b=parseInt(prompt("Enter breadth of rectangle : "));
+console.log(`Area = ${l*b}`);
+console.log(`Perimeter = ${2*(l+b)}`);

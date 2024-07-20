19e758e97c32473fd06971521cf0759105852268
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-07-20 12:18:17 +0530

    update Javascript Code/Binary Search Tree/Delete Node (Recursively).js

--- Javascript Code/Binary Search Tree/Delete Node (Recursively).js
+++ Javascript Code/Binary Search Tree/Delete Node (Recursively).js
@@ -1,30 +1,20 @@
 /*
-  Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
-
-  Basically, the deletion can be divided into two stages:
-    Search for a node to remove.
-    If the node is found, delete the node.
+  Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
 */
-var deleteNode = function(root, key) {
-  if(!root)
-    return null;
-  if(root.val===key){
-    if(!root.left)
-      return root.right;
-    if(!root.right)
-      return root.left;
-    let max=finMax(root.left);
-    root.val=max;
-    root.left=deleteNode(root.left,max);
-  }
-  else if(root.val>key)
-    root.left=deleteNode(root.left,key)
-  else
-    root.right=deleteNode(root.right,key);
-  return root;
+var kthSmallest = function(root, k) {
+  let ans=null;
+  let cnt=0;
+  function solve(node){
+    if(!node || ans)
+      return ;
+    solve(node.left);
+    cnt++;
+    if(cnt===k){
+      ans=node.val;
+      return ;
+    }
+    solve(node.right);
+  };
+  solve(root);
+  return ans;
 };
-function finMax(root){
-  if(!root.right)
-    return root.val;
-  return finMax(root.right);
-}

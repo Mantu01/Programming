eb73589eb5f6074eb15988a03183d94eb8d80072
Mantu Kumar <149353348+Mantu01@users.noreply.github.com>
2024-07-23 14:52:05 +0530

    update Javascript Code/Binary Search Tree/BST iterator.js

--- Javascript Code/Binary Search Tree/BST iterator.js
+++ Javascript Code/Binary Search Tree/BST iterator.js
@@ -1,37 +1,19 @@
-/*
-  Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
-
-  BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
-  boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
-  int next() Moves the pointer to the right, then returns the number at the pointer.
-  Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
-
-  You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
-*/
-
-var BSTIterator = function(root) {
-  this.st=[];
-  this.push_left(root);
-};
-
-BSTIterator.prototype.push_left = function(node) {
-  while(node){
-    this.st.push(node);
-    node=node.left;
+var findMode = function(root) {
+  let counts=new Map();
+  let maxCnt=0;
+  function IOT(node){
+    if(!node)
+      return ;
+    counts.set(node.val,(counts.get(node.val) || 0)+1);
+    maxCnt=Math.max(maxCnt,counts.get(node.val));
+    IOT(node.left);
+    IOT(node.right);
+  };
+  IOT(root);
+  let ans=[];
+  for(let [key,val] of counts){
+    if(val===maxCnt)
+      ans.push(key);
   }
-}
-/**
- * @return -{number}
- */
-BSTIterator.prototype.next = function() {
-  let temp=this.st.pop();
-  if(temp.right)
-    this.push_left(temp.right);
-  return temp.val;
-};
-/**
- * @return -{boolean}
- */
-BSTIterator.prototype.hasNext = function() {
-  return this.st.length;
+  return ans;
 };
